b39dff1a99efb07d9518d032b947f19060129e7d
Matheus Thurler <matheusthurlernf@gmail.com>
2025-09-19 13:12:09 -0300

    teste de ping com runner

--- github-runner/entrypoint.sh
+++ github-runner/entrypoint.sh
@@ -2,7 +2,13 @@
 set -e
 
 
-./config.sh --url ${RUNNER_URL} --labels "ubuntu,docker" --name ${GITHUB_REPO} --token ${RUNNER_TOKEN}
+
+echo "Removendo o runner..."
+
+./config.sh remove --unattended --token "${RUNNER_TOKEN}"
+
+
+./config.sh --url ${RUNNER_URL} --work "_work" --unattended --replace --labels "ubuntu,docker" --name ${RUNNER_NAME} --token ${RUNNER_TOKEN}
 
 # Inicia o runner e mantém o container em execução
 ./run.sh
