3ef1dda19a14eda672a8c81726cd0f6481467d42
Matheus Thurler <matheusthurlernf@gmail.com>
2025-09-25 15:13:15 -0300

    test

--- modules/openvpn/openvpn.tf
+++ modules/openvpn/openvpn.tf
@@ -34,36 +34,36 @@ resource "openstack_networking_floatingip_associate_v2" "openvpn_ip" {
 
 # 
 
-resource "null_resource" "download_ovpn" {
-  depends_on = [
-    openstack_networking_floatingip_associate_v2.openvpn_ip
-  ]
+# resource "null_resource" "download_ovpn" {
+#   depends_on = [
+#     openstack_networking_floatingip_associate_v2.openvpn_ip
+#   ]
 
-  triggers = {
-    instance_ip = openstack_networking_floatingip_v2.openvpn_ip.address
-  }
+#   triggers = {
+#     instance_ip = openstack_networking_floatingip_v2.openvpn_ip.address
+#   }
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      echo "Aguardando a instância em ${self.triggers.instance_ip} ficar pronta..."
+#   provisioner "local-exec" {
+#     command = <<-EOT
+#       echo "Aguardando a instância em ${self.triggers.instance_ip} ficar pronta..."
 
-      # Espera o cloud-init terminar (usando 'sh' e o caminho relativo para a chave)
-      timeout 300 sh -c '
-        while ! ssh -i ./id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=10 ubuntu@${self.triggers.instance_ip} "sudo cloud-init status --wait"; do
-          echo "Aguardando cloud-init finalizar..."
-          sleep 10
-        done
-      '
+#       # Espera o cloud-init terminar (usando 'sh' e o caminho relativo para a chave)
+#       timeout 300 sh -c '
+#         while ! ssh -i ./id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=10 ubuntu@${self.triggers.instance_ip} "sudo cloud-init status --wait"; do
+#           echo "Aguardando cloud-init finalizar..."
+#           sleep 10
+#         done
+#       '
       
-      echo "Instância pronta. Baixando client.ovpn..."
+#       echo "Instância pronta. Baixando client.ovpn..."
       
-      # Baixa o arquivo (usando o caminho relativo para a chave)
-      set -e 
-      scp -i ./id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=10 \
-        ubuntu@${self.triggers.instance_ip}:/home/ubuntu/client.ovpn \
-        ./client.ovpn
+#       # Baixa o arquivo (usando o caminho relativo para a chave)
+#       set -e 
+#       scp -i ./id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=10 \
+#         ubuntu@${self.triggers.instance_ip}:/home/ubuntu/client.ovpn \
+#         ./client.ovpn
         
-      echo "Download concluído com sucesso!"
-    EOT
-  }
-}
+#       echo "Download concluído com sucesso!"
+#     EOT
+#   }
+# }
