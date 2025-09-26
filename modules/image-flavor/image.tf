73a82bfc0e70800a3ff94c2e2c7bca52cf4e271f
Matheus Thurler <matheusthurlernf@gmail.com>
2025-09-26 16:23:54 -0300

    test

--- modules/image-flavor/image.tf
+++ modules/image-flavor/image.tf
@@ -1,7 +1,7 @@
-# resource "openstack_images_image_v2" "ubuntu_image" {
-#   name             = "Ubuntu 24.04"
-#   image_source_url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
-#   container_format = "bare"
-#   disk_format      = "qcow2"
-# }
+resource "openstack_images_image_v2" "ubuntu_image" {
+  name             = "Ubuntu 24.04"
+  image_source_url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  container_format = "bare"
+  disk_format      = "qcow2"
+}
 
